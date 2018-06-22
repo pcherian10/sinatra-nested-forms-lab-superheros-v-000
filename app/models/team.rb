1ab7b7d231575a0b63d46fef3fa2742c2c5e1ad6
Paul <pcherian10@gmail.com>
2018-06-22 02:48:46 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,13 @@
 class Team
   attr_accessor :name, :motto
-
+  @@teams
   def initialize (params)
     @name, @motto = params[:name], params[:motto]
+    @@teams << self
+  end
+
+  def self.all
+    @@teams
   end
 
 end
